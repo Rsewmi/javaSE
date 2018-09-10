92757d8620062972c654e8ad51c5e8c6f612a096
sewmi <sewmirupasinghe@gmail.com>
2018-09-10 21:08:06 +0530

    initial commit

---
+++ Sales/src/Login.java
@@ -0,0 +1,13 @@
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Login.login();
+	}
+	
+	public static void login() {
+		System.out.println("login okay");
+	}
+
+}
