1b69d10de499c7c64d5109aec8a60d30ae95b44a
kaweesha <wkaweesh@gamil.com>
2018-09-10 21:34:57 +0530

    commiting logOut method

--- Sales/src/Login.java
+++ Sales/src/Login.java
@@ -4,10 +4,15 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Login.login();
+		logOut();
 	}
 	
 	public static void login() {
 		System.out.println("login okay");
+	}
+
+	public static void logOut() {
+		System.out.println("logged out");
 	}
 
 }
